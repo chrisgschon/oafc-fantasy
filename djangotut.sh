23b02d828800503b99b8131694f6276113e54880
Chris Schon <chrisschon@Chriss-MacBook-Pro.local>
2019-01-27 15:22:09 +0000

    up to step 5 of tutorial

--- djangotut.sh
+++ djangotut.sh
@@ -1,2 +1,4 @@
-django-admin startproject mobileBackend && cd mobileBackend
-python manage.py startapp oafcff
+django-admin startproject backend
+python manage.py startapp players
+python manage.py migrate
+python manage.py runserver
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chrisschon/Git/oafc-fantasy/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
